2477997e5e1d13970d1b7d0cec4e07330df42ba0
crispy-pepper <sunni.xue@gmail.com>
2024-11-16 17:18:58 -0500

    main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame as pg
+import time
+import random as rand
+
+pg.init()
+pg.font.init()
+
+clock = pg.time.Clock()
+
+
+
+screenW,screenH = pg.display.Info().current_w-100, pg.display.Info().current_h-100
+screen = pg.display.set_mode((screenW,screenH))
+
+pg.display.set_caption("Ascend The Stars!")
+
+
+def updateScreen():
+    pass
+
+
+
+
+mainloop = True
+
+while mainloop:
+    
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            mainloop = False
+            exit()
+
+
+    updateScreen()
