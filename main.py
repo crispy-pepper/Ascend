d9deb0ef15524de2a8283a666e8d8bc8421936f5
crispy-pepper <sunni.xue@gmail.com>
2024-11-17 00:17:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame as pg
 import time
 import random as rand
 import sys
+rand.seed(420)
 
 
 pg.init()
@@ -17,37 +18,79 @@ screen = pg.display.set_mode((screenW,screenH))
 pg.display.set_caption("Ascend The Stars!")
 
 class Character(object):
-    def __init__(self,name,image,width,height):
-        self.name = name
-        self.image = image
-        self.width = width
-        self.height = height
-
-    def draw():
-        pass
-
-class CurrentCharacter(Character):
-    def __init__(self,name,image,width,height,pos,speed):
+    def __init__(self,name,image,width,height,pos):
         self.name = name
         self.image = pg.image.load(image)
         self.width = width
         self.height = height
         self.x,self.y = pos
+
+    def draw(self):
+        self.image = pg.transform.scale(self.image, (self.width, self.height))
+        screen.blit(self.image, (self.x, self.y))
+
+    def Rect(self):
+        return pg.Rect(self.x,self.y,self.width,self.height)
+
+class CurrentCharacter(Character):
+    def __init__(self,name,image,width,height,pos,speed):
+        self.name = name
+        self.image = pg.image.load(image)
+        self.images = [self.image]
+        self.width = width
+        self.height = height
+        self.x,self.y = pos
         self.speed = speed
+        self.rect = self.image.get_rect()
+        self.th = self.height
+        self.tw = self.width
 
+    def Rect(self):
+        self.rect = pg.Rect(self.x,self.y,self.tw,self.th)
+        print(self.rect)
+        return self.rect
+    
     def run(self):
-        if keys[pg.K_LEFT] and self.x > 0+self.width:self.x -= self.speed
+        self.rect = self.Rect()
+        if keys[pg.K_LEFT] and self.x > 0+self.width:
+            self.x -= self.speed
+            for i in self.images[1:]:
+                i.x -= self.speed
             
-        if keys[pg.K_RIGHT] and self.x < screenW - self.width:self.x += self.speed
+            
+        if keys[pg.K_RIGHT] and self.x < screenW - self.width:
+            self.x += self.speed
+            for i in self.images[1:]:
+                i.x += self.speed
 
-        if keys[pg.K_UP] and self.y > 0+self.height:self.y -= self.speed
+        if keys[pg.K_UP] and self.y > 0+self.height:
+            self.y -= self.speed
+            for i in self.images[1:]:
+                i.y -= self.speed
 
-        if keys[pg.K_DOWN] and self.y < screenH - self.height:self.y += self.speed
+        if keys[pg.K_DOWN] and self.y < screenH - self.height:
+            self.y += self.speed
+            for i in self.images[1:]:
+                i.y += self.speed
+
+
+        for x in collidables:
+            if self.rect.colliderect(x.Rect()):
+                self.images.append(x)
+                self.tw += x.width
+                self.th += x.height
+            
+                collidables.remove(x)
+                print("collide")
 
         
     def draw(self,screen):
         self.image = pg.transform.scale(self.image, (self.width, self.height))
         screen.blit(self.image, (self.x, self.y))
+
+        for i in self.images[1:]:
+            i.image = pg.transform.scale(i.image, (i.width, i.height))
+            screen.blit(i.image, (i.x, i.y))
 
         
 
@@ -69,6 +112,13 @@ class Button(object):
                 if pg.mouse.get_pressed()[0]:
                     self.function()
 
+def generate(name,image, integer, rangex,rangey, width,height):
+    global collidables
+
+    for x in range(integer):
+        collidables.append(Character(name,image,width,height,(rand.randint(rangex[0]+width,rangex[1]-width),rand.randint(rangey[0]+height,rangey[1]-height))))
+
+
 
 def drawBackground(image):
     image = pg.image.load(image)
@@ -78,6 +128,9 @@ def drawBackground(image):
 
 def updateScreen():
     drawBackground(currentBackground)
+    for x in collidables:
+        x.draw()
+        
     player.run()
     player.draw(screen)
 
@@ -87,8 +140,10 @@ def updateScreen():
 mainloop = True
 
 
-player = CurrentCharacter("name","C:\\Users\\Oat_M\\Dropbox\\PC\\Documents\\GitHub\\Ascend\\assets\\Bottom.jpg",25,25,(0,0),2)
+player = CurrentCharacter("name","C:\\Users\\Oat_M\\Dropbox\\PC\\Documents\\GitHub\\Ascend\\assets\\Bottom.jpg",25,25,(screenW//2,screenH//2),2)
 currentBackground = "C:\\Users\\Oat_M\\Dropbox\\PC\\Documents\\GitHub\\Ascend\\assets\\Section1.png"
+collidables = []
+generate("name","C:\\Users\\Oat_M\\Dropbox\\PC\\Documents\\GitHub\\Ascend\\assets\\Bottom.jpg",30,(0,screenW),(0,screenH),20,20)
 
 while mainloop:
     clock.tick(500)
